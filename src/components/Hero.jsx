a41b7f37701f6b93b54713af57bb5db7b558c247
Dave <sololadavid4@gmail.com>
2024-07-20 18:37:13 +0100

    initial commit

---
+++ src/components/Hero.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Hero() {
+  return (
+    <div className=" text-left mx-20 mt-20 bg-white ">
+      <div className=" text-lg text-baseContentSecondary  font-poppinsRegular">
+        Hey, I'm Luca 👋🏻
+      </div>
+      <div className="  text-7xl mt-4 text-baseContent font-poppinsBold w-64 ">
+        <span className=" text-buttonColor ">Full</span>stack Developer
+      </div>
+    </div>
+  );
+}
+
+export default Hero;
