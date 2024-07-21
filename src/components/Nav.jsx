a6fb9a1f90de35e69dc790f2d8c7a3e341bf7ec8
Dave <sololadavid4@gmail.com>
2024-07-21 13:25:27 +0100

    update

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -3,26 +3,26 @@ import React from "react";
 function Nav() {
   const navLinks = [
     { href: "#dashboard", label: "Home", current: true },
-    { href: "#Team", label: "Project", current: false },
+    { href: "#Team", label: "Projects", current: false },
     { href: "#Team", label: "About", current: false },
   ];
   return (
-    <div className=" flex  justify-between items-center mx-20  mt-4">
+    <div className=" flex  justify-between items-center   mt-4">
       <div className=" font-poppinsSemiBold text-md flex">
         Solola David
         <div className=" bg-buttonColor rounded-full mt-[13px] ml-[0.5px]  h-1 w-1"></div>
       </div>
       <div>
-        <ul className=" flex gap-6 font-poppinsSemiBold text-md ">
+        <ul className=" hidden md:flex gap-6 font-poppinsSemiBold text-md ">
           {navLinks.map((link) => (
             <li key={link.label}>
               <a
                 href={link.href}
-                className={
+                className={` hover:text-buttonColor ${
                   link.current
                     ? "text-buttonColor"
                     : "text-baseContentSecondary"
-                }
+                }`}
               >
                 {link.label}
               </a>
