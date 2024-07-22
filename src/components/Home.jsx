834ebd91b8058ebb7a931580a58bbe93e29ea7b7
Dave <sololadavid4@gmail.com>
2024-07-22 18:45:42 +0100

    page components

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,26 @@
 import React from "react";
 import Nav from "./Nav";
 import Hero from "./Hero";
+import About from "./About";
+import Projects from "./Projects";
 
 function Home() {
   return (
-    <div>
+    <div className="mx-6 lg:mx-14">
       <section>
         <Nav />
       </section>
 
       <section>
         <Hero />
+      </section>
+
+      <section className=" mb-5">
+        <About />
+      </section>
+
+      <section className=" mb-5">
+        <Projects  />
       </section>
     </div>
   );
