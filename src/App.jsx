996a11d7a1c759006ee641b86e8ee272c189edef
Dave <sololadavid4@gmail.com>
2024-07-22 23:55:06 +0100

    import

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import Home from "./components/Home";
+import Contact from "./components/Contacts";
+
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
   return (
-    <>
-      <Home />
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </Router>
   );
 }
 
