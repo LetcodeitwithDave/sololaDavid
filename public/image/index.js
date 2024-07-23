a6fa359caa5bbed5221848ef3e366f0756a0256b
Dave <sololadavid4@gmail.com>
2024-07-23 16:45:32 +0100

    add new componets

--- public/image/index.js
+++ public/image/index.js
@@ -2,11 +2,18 @@ import about from './About.jpg'
 import bookproject from './bookproject.png'
 import  jobproject from './jobproject.png'
 import profile from './image2.png'
-
+import frontend  from './frontend.png'
+import backend from './goal.png'
+import softskill from './softskill.png'
+import  webdesign from './webdesign.png'
 
 export {
     about,
     bookproject,
     jobproject,
-    profile
+    profile,
+    frontend,
+    backend,
+    webdesign,
+    softskill
 }
