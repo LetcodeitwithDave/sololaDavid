b0eb99a17734f9d22b204057831de13066e2bac1
Dave <sololadavid4@gmail.com>
2024-07-23 22:55:19 +0100

    first commit

---
+++ src/components/Reachout.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+
+function Reachout() {
+  return (
+    <div className="flex flex-col mt-28 justify-center items-center">
+      <h1 className="sm:text-4xl text-3xl   font-poppinsBold mt-16 text-white text-center">
+        Get In Touch
+      </h1>
+    </div>
+  );
+}
+
+// email svg
+
+// <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-mail"><rect width="20" height="16" x="2" y="4" rx="2"/><path d="m22 7-8.97 5.7a1.94 1.94 0 0 1-2.06 0L2 7"/></svg>
+
+// linkedin
+
+{
+  /* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-linkedin"><path d="M16 8a6 6 0 0 1 6 6v7h-4v-7a2 2 0 0 0-2-2 2 2 0 0 0-2 2v7h-4v-7a6 6 0 0 1 6-6z"/><rect width="4" height="12" x="2" y="9"/><circle cx="4" cy="4" r="2"/></svg> */
+}
+
+// twiter
+
+{
+  /* <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-twitter"><path d="M22 4s-.7 2.1-2 3.4c1.6 10-9.4 17.3-18 11.6 2.2.1 4.4-.6 6-2C3 15.5.5 9.6 3 5c2.2 2.6 5.6 4.1 9 4-.9-4.2 4-6.6 7-3.8 1.1 0 3-1.2 3-1.2z"/></svg> */
+}
+
+export default Reachout;
