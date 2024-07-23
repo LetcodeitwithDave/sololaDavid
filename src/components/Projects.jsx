23b8e70972fcc80efc8e8d03edbf0e82c6d5a517
Dave <sololadavid4@gmail.com>
2024-07-23 10:07:01 +0100

    update code

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,31 +1,58 @@
 import React from "react";
-import jobproject from "../../public/image/jobproject.png";
+import { jobproject, bookproject } from "../../public/image";
 
 function Projects() {
   return (
-    <div className=" flex flex-col items-center mt-28 justify-center">
-      <h1 className=" text-4xl items-center font-poppinsBold text-baseContent">
-        Selected Projects
+    <div className="flex flex-col items-center mt-28 justify-center">
+      <h1 className="text-4xl items-center font-poppinsBold text-baseContent">
+        Selected{" "}
+        <span span className="text-buttonColor">
+          Projects
+        </span>
       </h1>
-      <div className=" mt-4 flex gap-8">
-        <div className=" w-1/2 h-1/2 px-4 py-4  ">
-          <div className="px-9 py-6  bg-purple-500 rounded-lg">
-            <img src={jobproject} alt="" className=" rounded-lg" />
+      <div className="mt-4 flex flex-col justify-center lg:flex-row gap-8">
+        <div className="lg:w-1/2 lg:h-1/2 h-1/2 w-full px-4 py-4">
+          <div className="px-9 py-6 bg-purple-500 rounded-lg hover:bg-purple-600 transition duration-300">
+            <img
+              src={jobproject}
+              alt=""
+              className="rounded-lg transform hover:scale-105 transition duration-300"
+            />
           </div>
 
-          <div className=" mt-4">
-            <h1 className=" font-poppinsSemiBold  text-2xl py-2">Techlaunch</h1>
+          <div className="mt-4">
+            <h1 className="font-poppinsSemiBold text-2xl py-2 text-baseContent">
+              Techlaunch
+            </h1>
+          </div>
+          <div>
+            <p className="font-poppinsMedium text-lg text-baseContent">
+              Job board platform, enabling student get tech jobs based on their
+              schedule.
+            </p>
           </div>
         </div>
 
-        <div className=" w-1/2 h-96 border rounded-lg shadow-lg border-gray-300 bg-white ">
-          <div className="px-4 py-4  bg-gray-300">
-            <img src={jobproject} alt="" className=" rounded-lg" />
+        <div className="lg:w-1/2 lg:h-1/2 h-1/2 w-full px-4 py-4">
+          <div className="px-9 py-9 bg-purple-500 rounded-lg hover:bg-purple-600 transition duration-300">
+            <img
+              src={bookproject}
+              alt=""
+              className="rounded-lg object-cover transform hover:scale-105 transition duration-300"
+            />
+          </div>
+
+          <div className="mt-4">
+            <h1 className="font-poppinsSemiBold text-2xl py-2 text-baseContent">
+              BookHaven
+            </h1>
+          </div>
+          <div>
+            <p className="font-poppinsMedium text-lg text-baseContent">
+              E-commerce platform promoting eco-friendly books.
+            </p>
           </div>
         </div>
-        {/* <div className=' w-72 h-72 border shadow-lg border-gray-300 bg-white'>
-
-        </div> */}
       </div>
     </div>
   );
