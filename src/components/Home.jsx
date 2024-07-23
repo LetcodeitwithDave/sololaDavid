480e91a48922606389332f16626eae1ff068c0b8
Dave <sololadavid4@gmail.com>
2024-07-23 23:55:58 +0100

    add contact section to main page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,28 +4,35 @@ import Hero from "./Hero";
 import About from "./About";
 import Projects from "./Projects";
 import Skill from "./Skill";
+import Reachout from "./Reachout";
 
 function Home() {
   return (
-    <div className="mx-6 lg:mx-14">
-      <section>
-        <Nav />
-      </section>
+    <div>
+      <div className="mx-6 lg:mx-14">
+        <section>
+          <Nav />
+        </section>
 
-      <section>
-        <Hero />
-      </section>
+        <section>
+          <Hero />
+        </section>
 
-      <section className=" mb-5">
-        <About />
-      </section>
+        <section className=" mb-5">
+          <About />
+        </section>
 
-      <section className=" mb-5">
-        <Projects />
-      </section>
+        <section className=" mb-5">
+          <Projects />
+        </section>
 
-      <section className=" mb-5">
-        <Skill />
+        <section className=" mb-5">
+          <Skill />
+        </section>
+      </div>
+
+      <section className=" bg-buttonColor w-full">
+        <Reachout />
       </section>
     </div>
   );
