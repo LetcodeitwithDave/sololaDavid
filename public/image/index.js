76f9e325e3a4a09ba485bb15aac217e5d383eac9
Dave <sololadavid4@gmail.com>
2024-07-23 10:11:25 +0100

    static images

---
+++ public/image/index.js
@@ -0,0 +1,12 @@
+import about from './About.jpg'
+import bookproject from './bookproject.png'
+import  jobproject from './jobproject.png'
+import profile from './image2.png'
+
+
+export {
+    about,
+    bookproject,
+    jobproject,
+    profile
+}
