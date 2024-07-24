75ba2b966680cf204037faee3b25548f567c3f65
Dave <sololadavid4@gmail.com>
2024-07-24 13:03:31 +0100

    index

---
+++ src/sections/index.js
@@ -0,0 +1,15 @@
+import About from "./About";
+import Hero from "./Hero";
+import Nav from "./Nav";
+import Projects from "./Projects";
+import Reachout from "./Reachout";
+import Skill from "./Skill";
+
+export {
+    About,
+    Hero,
+    Nav,
+    Projects,
+    Reachout,
+    Skill,
+}
