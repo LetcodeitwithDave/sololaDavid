93e6093ac30c0a23fdf6de7de4a9185bf09dc483
Dave <sololadavid4@gmail.com>
2024-07-25 23:07:59 +0100

    work page

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Nav from "../sections/Nav";
+import { jobproject, bookproject } from "../../public/image";
 
 function Work() {
   return (
@@ -12,6 +13,54 @@ function Work() {
       <p className=" text-xl mt-4 font-poppinsMedium text-baseContentSecondary">
         Designing and Developing Robust and Stylish Web Applications
       </p>
+
+      <div className="mt-12 flex flex-col justify-center ">
+        <a href="/work/techlaunch">
+          <div className="h-full w-full px-1 py-1 lg:px-28 lg:py-4 ">
+            <div className="px-5 py-5 md:px-8 md:py-8 lg:px-9 lg:py-6  bg-purple-500 rounded-lg hover:bg-purple-600 transition duration-300">
+              <img
+                src={jobproject}
+                alt=""
+                className="rounded-lg transform hover:scale-105 transition duration-300"
+              />
+            </div>
+
+            <div className="mt-4">
+              <h1 className="font-poppinsBold text-3xl md:text-4xl lg:text-5xl py-2 text-baseContent">
+                Techlaunch
+              </h1>
+            </div>
+            <div>
+              <p className="font-poppinsMedium text-lg text-baseContent">
+                Job board platform, enabling student get tech jobs based on
+                their schedule.
+              </p>
+            </div>
+          </div>
+        </a>
+        <a href="">
+          <div className="h-full mt-12 w-full px-1 py-1 lg:px-28 lg:py-4">
+            <div className="px-5 py-5 md:px-8 md:py-8 lg:px-9 lg:py-6 bg-purple-500 rounded-lg hover:bg-purple-600 transition duration-300">
+              <img
+                src={bookproject}
+                alt=""
+                className="rounded-lg object-cover transform hover:scale-105 transition duration-300"
+              />
+            </div>
+
+            <div className="mt-9">
+              <h1 className="font-poppinsBold md:text-4xl  text-5xl py-2 text-baseContent">
+                BookHaven
+              </h1>
+            </div>
+            <div>
+              <p className="font-poppinsMedium text-lg text-baseContent">
+                E-commerce platform promoting eco-friendly books.
+              </p>
+            </div>
+          </div>
+        </a>
+      </div>
     </div>
   );
 }
