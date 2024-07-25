fbd2cf15b98daa3ec0011d4039612ed0b5bf0406
Dave <sololadavid4@gmail.com>
2024-07-25 20:17:50 +0100

    update

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -4,7 +4,7 @@ import { useLocation } from "react-router-dom";
 function Nav() {
   const navLinks = [
     { href: "/", label: "Home" },
-    { href: "#Team", label: "Works" },
+    { href: "/work", label: "Works" },
     { href: "/contact", label: "Contact" },
   ];
   const location = useLocation();
@@ -48,3 +48,27 @@ function Nav() {
 }
 
 export default Nav;
+
+// menu svg
+{
+  /* <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth="2"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                onClick={toggleSideNav}
+                className="lucide lucide-x md:hidden text-white mt-7  mr-2 ml-[276px]  "
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg> */
+}
+
+// arrow svg
+
+// <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-arrow-up-right"><path d="M7 7h10v10"/><path d="M7 17 17 7"/></svg>
