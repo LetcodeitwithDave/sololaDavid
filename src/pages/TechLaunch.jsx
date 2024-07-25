fa6e022505059fcb0515a07fb493c2ff1041443b
Dave <sololadavid4@gmail.com>
2024-07-25 22:29:28 +0100

    first commit

---
+++ src/pages/TechLaunch.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Nav } from "../sections";
+
+function TechLaunch() {
+  return (
+    <div className=" mx-6 lg:mx-14">
+      <div>
+        <Nav />
+      </div>
+    </div>
+  );
+}
+
+export default TechLaunch;
