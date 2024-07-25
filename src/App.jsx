e25e7d5b24a6b633ec3d4ee74dc138d4a75e2e45
Dave <sololadavid4@gmail.com>
2024-07-25 06:13:37 +0100

    debugging

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { Home, Contact } from "./pages";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
+  console.log("App component loaded");
   return (
     <Router>
       <Routes>
