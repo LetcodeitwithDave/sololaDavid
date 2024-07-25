d86abe71038e3f8fb0ba25abe8e7eacff301659f
Dave <sololadavid4@gmail.com>
2024-07-25 20:16:22 +0100

    update

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,9 +1,17 @@
 import React from "react";
+import Nav from "../sections/Nav";
 
 function Work() {
   return (
-    <div>
-      <div>work page</div>
+    <div className=" mx-6 lg:mx-14">
+      <Nav />
+
+      <div className=" mt-28 text-7xl max-w-4xl font-poppinsBold text-baseContent">
+        My <span className=" text-buttonColor">Best</span> Creations
+      </div>
+      <p className=" text-xl mt-4 font-poppinsMedium text-baseContentSecondary">
+        Designing and Developing Robust and Stylish Web Applications
+      </p>
     </div>
   );
 }
