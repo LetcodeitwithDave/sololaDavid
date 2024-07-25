edd138e3c8ed1e549fa67ca8cd29b9dcb9705c51
Dave <sololadavid4@gmail.com>
2024-07-25 23:11:12 +0100

    add route for TechLaunch project

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { Home, Contact, Work } from "./pages";
+import { Home, Contact, Work, TechLaunch } from "./pages";
 
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 function App() {
@@ -10,6 +10,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/work" element={<Work />} />
         <Route path="/contact" element={<Contact />} />
+        <Route path="/work/techlaunch" element={<TechLaunch />} />
       </Routes>
     </Router>
   );
