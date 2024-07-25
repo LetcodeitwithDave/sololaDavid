879d622040644cf425d25b3137f2bfadd9d36398
Dave <sololadavid4@gmail.com>
2024-07-25 23:08:22 +0100

    add work page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,11 @@
 import Home from "./Home";
 import Contact from "./Contacts";
 import Work from "./Work";
+import TechLaunch from "./TechLaunch";
 
 export {
     Home,
     Contact,
-    Work
+    Work,
+    TechLaunch
 }
