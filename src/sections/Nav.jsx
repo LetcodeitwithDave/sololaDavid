7f3e090ddf8f521530134fadc1f1e61e8330859a
Dave <sololadavid4@gmail.com>
2024-07-26 14:03:07 +0100

    update

--- src/sections/Nav.jsx
+++ src/sections/Nav.jsx
@@ -16,10 +16,12 @@ function Nav() {
   };
   return (
     <div className=" flex  justify-between items-center   mt-4">
-      <div className=" font-poppinsSemiBold text-md flex">
-        Solola David
-        <div className=" bg-buttonColor rounded-full mt-[13px] ml-[0.5px]  h-1 w-1"></div>
-      </div>
+      <a href="/">
+        <div className=" font-poppinsSemiBold text-md flex">
+          Solola David
+          <div className=" bg-buttonColor rounded-full mt-[13px] ml-[0.5px]  h-1 w-1"></div>
+        </div>
+      </a>
       <div>
         <ul className=" hidden md:flex gap-6 font-poppinsSemiBold text-md ">
           {navLinks.map((link) => (
