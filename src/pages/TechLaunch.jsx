a49b917e93e00d816c1863a81ffa811e4e1fdb8a
Dave <sololadavid4@gmail.com>
2024-07-26 14:00:30 +0100

    add project image with nice mockup

--- src/pages/TechLaunch.jsx
+++ src/pages/TechLaunch.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Nav, Reachout } from "../sections";
+import { jobMockup, bookMockup } from "../../public/image";
 
 function TechLaunch() {
   return (
@@ -9,10 +10,10 @@ function TechLaunch() {
           <Nav />
         </div>
         <div className=" mt-24 flex">
-          <h1 className=" text-6xl font-poppinsBold text-baseContent">
+          <h1 className=" text-5xl lg:text-6xl font-poppinsBold text-baseContent">
             TechLaunch
           </h1>
-          <div className=" bg-buttonColor rounded-full mt-[38px] ml-[0.5px]  h-4 w-4"></div>
+          <div className=" bg-buttonColor rounded-full mt-[28px] lg:mt-[38px] ml-[0.5px]  h-4 w-4"></div>
         </div>
 
         <div>
@@ -21,7 +22,20 @@ function TechLaunch() {
             schedule.
           </p>
         </div>
+
+        <div className="mt-12 flex flex-col justify-center ">
+          <div className="h-1/2 w-full  lg:px-28 lg:py-4 ">
+            <div>
+              <img
+                src={jobMockup}
+                alt=""
+                className=" rounded-md transform hover:scale-105 transition duration-300"
+              />
+            </div>
+          </div>
+        </div>
       </div>
+
       <div className=" bg-buttonColor">
         <Reachout />
       </div>
