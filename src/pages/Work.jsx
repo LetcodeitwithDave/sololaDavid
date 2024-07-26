086ee6964d1e4ce89719c6425c0356bcb1fd0b12
Dave <sololadavid4@gmail.com>
2024-07-26 14:01:45 +0100

    add project image with nice mockup

--- src/pages/Work.jsx
+++ src/pages/Work.jsx
@@ -1,13 +1,18 @@
 import React from "react";
 import Nav from "../sections/Nav";
-import { jobproject, bookproject } from "../../public/image";
+import {
+  jobproject,
+  bookproject,
+  jobMockup,
+  bookMockup,
+} from "../../public/image";
 
 function Work() {
   return (
     <div className=" mx-6 lg:mx-14">
       <Nav />
 
-      <div className=" mt-28 text-7xl max-w-4xl font-poppinsBold text-baseContent">
+      <div className=" mt-28 lg:text-7xl text-6xl max-w-4xl font-poppinsBold text-baseContent">
         My <span className=" text-buttonColor">Best</span> Creations
       </div>
       <p className=" text-xl mt-4 font-poppinsMedium text-baseContentSecondary">
@@ -16,14 +21,21 @@ function Work() {
 
       <div className="mt-12 flex flex-col justify-center ">
         <a href="/work/techlaunch">
-          <div className="h-full w-full px-1 py-1 lg:px-28 lg:py-4 ">
-            <div className="px-5 py-5 md:px-8 md:py-8 lg:px-9 lg:py-6  bg-purple-500 rounded-lg hover:bg-purple-600 transition duration-300">
+          <div className="h-1/2 w-full  lg:px-28 lg:py-4 ">
+            <div>
+              <img
+                src={jobMockup}
+                alt=""
+                className=" rounded-md transform hover:scale-105 transition duration-300"
+              />
+            </div>
+            {/* <div className="px-5 py-5 md:px-8 md:py-8 lg:px-9 lg:py-6  bg-purple-500 rounded-lg hover:bg-purple-600 transition duration-300">
               <img
                 src={jobproject}
                 alt=""
                 className="rounded-lg transform hover:scale-105 transition duration-300"
               />
-            </div>
+            </div> */}
 
             <div className="mt-4">
               <h1 className="font-poppinsBold text-3xl md:text-4xl lg:text-5xl py-2 text-baseContent">
@@ -39,14 +51,22 @@ function Work() {
           </div>
         </a>
         <a href="">
-          <div className="h-full mt-12 w-full px-1 py-1 lg:px-28 lg:py-4">
-            <div className="px-5 py-5 md:px-8 md:py-8 lg:px-9 lg:py-6 bg-purple-500 rounded-lg hover:bg-purple-600 transition duration-300">
+          <div className="h-full mt-12 md:mt-20 w-full px-1 py-1 lg:px-28 lg:py-4">
+            <div>
+              <img
+                src={bookMockup}
+                alt=""
+                className=" rounded-md transform hover:scale-105 transition duration-300"
+              />
+            </div>
+
+            {/* <div className="px-5 py-5 md:px-8 md:py-8 lg:px-9 lg:py-6 bg-purple-500 rounded-lg hover:bg-purple-600 transition duration-300">
               <img
                 src={bookproject}
                 alt=""
                 className="rounded-lg object-cover transform hover:scale-105 transition duration-300"
               />
-            </div>
+            </div> */}
 
             <div className="mt-9">
               <h1 className="font-poppinsBold md:text-4xl  text-5xl py-2 text-baseContent">
